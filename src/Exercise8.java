df0c9e587cd530f567ce8e6f98a3f24ee6a9c17a
Momi.lv <ilga.miezaine@gmail.com>
2023-03-07 22:23:41 +0200

    adding homework exercise8

---
+++ src/Exercise8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        System.out.println("Calculator");
+        System.out.println("Enter first number: ");
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        System.out.println("Enter second number: ");
+        b = input.nextInt();
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+        int sub = a - b;
+        System.out.println("Sub is: " + sub);
+        int multi = a * b;
+        System.out.println("Multiplication is: " + multi);
+        int div = a / b;
+        System.out.println("Division is: " + div);
+
+
+
+
+
+    }
+}
