2c42d45239ba4ed6e0fa6cd96506bf533b791a5c
Momi.lv <ilga.miezaine@gmail.com>
2023-03-08 19:58:39 +0200

    changed reference names

--- src/com/sda/she_likes_java/homework/Exercise8.java
+++ src/com/sda/she_likes_java/homework/Exercise8.java
@@ -6,19 +6,19 @@ public class Exercise8 {
     public static void main(String[] args) {
         System.out.println("Calculator");
         System.out.println("Enter first number: ");
-        int a;
-        int b;
+        int Num1;
+        int Num2;
         Scanner input = new Scanner(System.in);
-        a = input.nextInt();
+        Num1 = input.nextInt();
         System.out.println("Enter second number: ");
-        b = input.nextInt();
-        int sum = a + b;
+        Num2 = input.nextInt();
+        int sum = Num1 + Num2;
         System.out.println("Sum is: " + sum);
-        int sub = a - b;
+        int sub = Num1 - Num2;
         System.out.println("Sub is: " + sub);
-        int multi = a * b;
+        int multi = Num1 * Num2;
         System.out.println("Multiplication is: " + multi);
-        double div = (double) a / b;
+        double div = (double) Num1 / Num2;
         System.out.println("Division is: " + div);
 
 
