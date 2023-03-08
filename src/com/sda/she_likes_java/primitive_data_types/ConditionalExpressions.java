fe7d30e833add077dee73af5177edc7bc5f14ab5
Momi.lv <ilga.miezaine@gmail.com>
2023-03-08 21:30:33 +0200

    conditional statement - example with dynamic boolean expression

--- src/com/sda/she_likes_java/primitive_data_types/ConditionalExpressions.java
+++ src/com/sda/she_likes_java/primitive_data_types/ConditionalExpressions.java
@@ -1,8 +1,11 @@
 package com.sda.she_likes_java.primitive_data_types;
 
+import java.util.Scanner;
+
 public class ConditionalExpressions {
     public static void main(String[] args) {
         System.out.println("Conditionals :)");
+
         int age = 15;
         boolean isKid = age < 18;
         System.out.println("is kid: " + isKid);
@@ -14,5 +17,16 @@ public class ConditionalExpressions {
             System.out.println("This will be run when condition is false");
         }
 
+        System.out.println();
+
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Tell me how old are You");
+        int personAge = inputReader.nextInt();
+
+        if (personAge >= 18) {
+            System.out.println("Hello Lady");
+        } else {
+            System.out.println("It's time to go to sleet kid :)");
+        }
     }
 }
