da49ce99d64659c4a5d0608733120bec7fb70ca9
Momi.lv <ilga.miezaine@gmail.com>
2023-03-10 21:04:39 +0200

    negation operator explained - a little bit partII

--- src/com/sda/she_likes_java/operators/Operators.java
+++ src/com/sda/she_likes_java/operators/Operators.java
@@ -2,13 +2,12 @@ package com.sda.she_likes_java.operators;
 
 public class Operators {
     public static void main(String[] args) {
-        System.out.println("Now, let's try some oparators");
+        System.out.println("Now, let's try some operators");
         System.out.println("Logical operators");
 
         boolean firstValue;
         boolean secondValue;
         boolean evaluationResult;
-
         System.out.println("&& operator");
         firstValue = true;
         secondValue = true;
@@ -31,7 +30,7 @@ public class Operators {
         System.out.println("false && false: " + evaluationResult);
 
         System.out.println();
-        System.out.println("Let's Test negation operator");
+        System.out.println("Let's test negation operator");
         firstValue = false;
         secondValue = !firstValue;
         System.out.println("!false is: " + secondValue);
@@ -39,5 +38,16 @@ public class Operators {
         firstValue = true;
         secondValue = !firstValue;
         System.out.println("!true is: " + secondValue);
+
+        // negation of isPolite equals naughty
+        boolean isPolite = false;
+        if (!isPolite) {
+            System.out.println("You're not polite, or just naughty");
+        }
+
+        boolean naughty = true;
+        if (naughty) {
+            System.out.println("You're not polite, or just naughty");
+        }
     }
 }
