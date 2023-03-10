1c9b40922beb63d86d94d761379099e9b56e6a1d
Momi.lv <ilga.miezaine@gmail.com>
2023-03-10 20:14:34 +0200

    mariusz version3 of bouncer in disco

--- src/com/sda/she_likes_java/homework/BouncerDiscoVersion3.java
+++ src/com/sda/she_likes_java/homework/BouncerDiscoVersion3.java
@@ -2,31 +2,24 @@ package com.sda.she_likes_java.homework;
 
 import java.util.Scanner;
 
-public class BouncerInDiscoVersions2 {
+public class BouncerDiscoVersion3 {
     public static void main(String[] args) {
         System.out.println("party time???");
 
         System.out.println("Somebody is coming to club");
         System.out.println("Hi, how old are you: ");
+
         Scanner inputReader = new Scanner(System.in);
         String declineMessage = "Sorry, go home";
         int age = inputReader.nextInt();
+        System.out.println("Let me know Your height now: ");
+        int height = inputReader.nextInt();
+        boolean isAllowedToParty = age >=18 && height <= 200;
 
-
-        if (age >= 18) {
-            System.out.println("Ok, so you are adult");
-            System.out.println("Let me know Your heigh: ");
-            int height = inputReader.nextInt();
-            if (height <= 200) {
-                System.out.println("Great!!!! Welcome to the party");
-            } else {
-                System.out.println(declineMessage);
-            }
+        if (isAllowedToParty) {
+            System.out.println("Have a great time at the party!!!" );
         } else {
             System.out.println(declineMessage);
         }
     }
 }
-
-
-
