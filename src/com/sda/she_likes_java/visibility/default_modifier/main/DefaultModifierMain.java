1687f10c67ab828078564c4a620944ca185d8c42
Momi.lv <ilga.miezaine@gmail.com>
2023-04-19 20:09:01 +0300

    default class

---
+++ src/com/sda/she_likes_java/visibility/default_modifier/main/DefaultModifierMain.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.visibility.default_modifier.main;
+
+import com.sda.she_likes_java.visibility.public_modifier.cars.Car;
+
+public class DefaultModifierMain {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+    }
+}
