5b087fc10bfd33bad0238fe8f4427a982a6419ed
Momi.lv <ilga.miezaine@gmail.com>
2023-04-21 21:44:32 +0300

    extends

---
+++ src/com/sda/she_likes_java/polymorphism/Cat.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class Cat extends Animal {
+        public void makeSound() {
+            System.out.println("miau");
+
+    }
+public void sleep() {
+    System.out.println("Cat is sleeping");
+}
+}
---
+++ src/com/sda/she_likes_java/polymorphism/Main.java
@@ -0,0 +1,27 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal someAnimal = new Animal();
+        Cat myCat = new Cat();
+        Animal secondCat = new Cat();
+        Animal dog = new Dog();
+        dog.makeSound();
+        myCat.makeSound();
+
+        callAnimal(someAnimal);
+        callAnimal(myCat);
+        callAnimal(dog);
+        callAnimal(new Elephant());
+
+        Object anotherCat = new Cat();
+        Animal animalCat = new Cat();
+        Cat thirdCat = new Cat();
+
+
+        }
+        public static void callAnimal(Animal animal){
+        animal.makeSound();
+        }
+    }
+
