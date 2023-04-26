ec5f39f510969f2336d64a384044495abe84f7ad
Momi.lv <ilga.miezaine@gmail.com>
2023-04-26 18:57:41 +0300

    homework exercise 20

---
+++ src/com/sda/she_likes_java/homework/exercise20/Main.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.homework.exercise20;
+
+public class Main {
+        public static void main(String[] args) {
+            CarUtils.safeCast(new Mazda());
+            CarUtils.safeCast(new Toyota());
+            CarUtils.safeCast(new Ferrari());
+        }
+    }
+}
