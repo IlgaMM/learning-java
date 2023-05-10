b87119db9c0cd02e1c60bec2aa948d6b595610b8
Momi.lv <ilga.miezaine@gmail.com>
2023-05-10 20:29:14 +0300

    List examples

--- src/com/sda/she_likes_java/collections/maps/Main.java
+++ src/com/sda/she_likes_java/collections/maps/Main.java
@@ -14,29 +14,31 @@ public class Main {
         Map<String, Trousers> myJeans = new HashMap<>();
         //key: "my favourite jeans"
         //value: new trousers("jeans","big star"
-        myJeans.put("my favourite jeans",new Trousers("jeans","big star"));
-        myJeans.put("my old one", new Trousers("unknown material","primemark"));
-        myJeans.put("my old one", new Trousers("unknown material","primemark II"));
+        myJeans.put("my favourite jeans", new Trousers("jeans", "big star"));
+        myJeans.put("my old one", new Trousers("unknown material", "primemark"));
+        myJeans.put("my old one", new Trousers("unknown material", "primemark II"));
         printMyJeans(myJeans);
 
-        Map<Person,Trousers> personsTrousers = new HashMap<>();
+        Map<Person, Trousers> personsTrousers = new HashMap<>();
         personsTrousers.put(new Person("mariusz", "p."), new Trousers("polish material", "primemark"));
         personsTrousers.put(new Person("karolina", "p."), new Trousers("polish material", "primemark"));
         System.out.println("persons trousers: " + personsTrousers);
     }
-    public static void printMyJeans(Map<String,Trousers> trousersMap){
+
+    public static void printMyJeans(Map<String, Trousers> trousersMap) {
         System.out.println("Lets start with no keys");
         System.out.println("key=[my favourite jeans]" + trousersMap.get("my favourite jeans"));
         System.out.println("key=[stupid key]" + trousersMap.get("stupid key"));
 
-        System.out.println("Lets interate the map");
+        System.out.println("Lets iterate the map");
         //Map.Entry represent key +value at once
-        System.out.println();{
-        for (Map.Entry<String,Trousers> item : trousersMap.entrySet()) {
-            System.out.println("key: " + item.getKey());
-            System.out.println("Value: " + item.getValue());
+        System.out.println();
+        {
+            for (Map.Entry<String, Trousers> item : trousersMap.entrySet()) {
+                System.out.println("key: " + item.getKey());
+                System.out.println("Value: " + item.getValue());
 
-        }
+            }
 
         }
     }
---
+++ src/com/sda/she_likes_java/homework/exercise23/package-info.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.homework.exercise23;
+/** Exercise 23:
+        Create a lesson journal:
+        - it should provide 3 methods
+        - provide a list of pupils
+        - provide a list of subjects
+        - provide the pupil’s grades in the subject.
+        Think about what classes you will need.
+        What structures will you use to store the data: list, map, set.
+ */
