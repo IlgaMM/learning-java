8af65d430810cc30c732687bf47931a8e2869b48
Momi.lv <ilga.miezaine@gmail.com>
2023-05-10 17:30:44 +0300

    map example - iteration

--- src/com/sda/she_likes_java/collections/maps/Main.java
+++ src/com/sda/she_likes_java/collections/maps/Main.java
@@ -17,9 +17,27 @@ public class Main {
         myJeans.put("my favourite jeans",new Trousers("jeans","big star"));
         myJeans.put("my old one", new Trousers("unknown material","primemark"));
         myJeans.put("my old one", new Trousers("unknown material","primemark II"));
+        printMyJeans(myJeans);
 
         Map<Person,Trousers> personsTrousers = new HashMap<>();
         personsTrousers.put(new Person("mariusz", "p."), new Trousers("polish material", "primemark"));
         personsTrousers.put(new Person("karolina", "p."), new Trousers("polish material", "primemark"));
+        System.out.println("persons trousers: " + personsTrousers);
+    }
+    public static void printMyJeans(Map<String,Trousers> trousersMap){
+        System.out.println("Lets start with no keys");
+        System.out.println("key=[my favourite jeans]" + trousersMap.get("my favourite jeans"));
+        System.out.println("key=[stupid key]" + trousersMap.get("stupid key"));
+
+        System.out.println("Lets interate the map");
+        //Map.Entry represent key +value at once
+        System.out.println();{
+        for (Map.Entry<String,Trousers> item : trousersMap.entrySet()) {
+            System.out.println("key: " + item.getKey());
+            System.out.println("Value: " + item.getValue());
+
+        }
+
+        }
     }
 }
