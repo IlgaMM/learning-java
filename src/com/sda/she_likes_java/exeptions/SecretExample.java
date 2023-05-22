f062e46a989651cc850ee582b527eb6597d19005
Momi.lv <ilga.miezaine@gmail.com>
2023-05-22 21:35:57 +0300

    Catch all example

--- src/com/sda/she_likes_java/exeptions/SecretExample.java
+++ src/com/sda/she_likes_java/exeptions/SecretExample.java
@@ -7,17 +7,19 @@ public class SecretExample {
         );
         System.out.println("I know Your secret now!!!");
         Secret enemySecret = secretStorage.tellMeYourSecret();
-        System.out.println("Enemy biggest secret was: " +enemySecret);
+        System.out.println("Enemy biggest secret was: " + enemySecret);
 
         try {
             System.out.println("Inside try block");
-           Secret enemysSecret = secretStorage.decryptMessageWithHSM();
+            Secret enemysSecret = secretStorage.decryptMessageWithHSM();
             System.out.println("Enemy's secret: " + enemysSecret);
         } catch (Exception e) {
             System.out.println("Unexpected exception: " + e.getMessage());
             e.printStackTrace();
         }
         System.out.println("After problematic source code");
+
+        Exception exception = new DecryptionIssueException("no reason");
 
         System.out.println();
         System.out.println("Let's play again with our own exceptions");
@@ -28,5 +30,14 @@ public class SecretExample {
         } catch (DecryptionIssueException e) {
             System.out.println("Cannot decrypt enemy's secret: " + e.getMessage());
         }
+        System.out.println("Now with single catch");
+        try {
+            secretStorage.decrypt();
+        } catch (DecryptionIssueException e) {
+            System.out.println("Cannot decrypt enemy's secret: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Generic catch all :)");
+        }
     }
 }
+
