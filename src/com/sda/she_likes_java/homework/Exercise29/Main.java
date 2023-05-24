fc56888ae7069eaf6b162e00cb5036e38646ee3c
Momi.lv <ilga.miezaine@gmail.com>
2023-05-24 21:40:01 +0300

    sql

--- src/com/sda/she_likes_java/homework/Exercise29/Main.java
+++ src/com/sda/she_likes_java/homework/Exercise29/Main.java
@@ -6,8 +6,17 @@ import static com.sda.she_likes_java.homework.Exercise29.PostalCodeChecker.check
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        char mLetter = 'm';
+        System.out.println("m letter: " + mLetter);
+        int mLetterCode = mLetter;
+        System.out.println("m letter code: " + mLetterCode);
 
+        String name = "mariusz";
+        System.out.println("First 3 letter: " + name.substring(0,3));
+
+
+        System.out.println("Lets check postal codes");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter postal code: ");
         String postalCodeValue = scanner.nextLine();
 
@@ -15,7 +24,7 @@ public class Main {
 
         try {
             checkPostalCode(postalCode);
-            System.out.println("Postal code format ir correct");
+            System.out.println("Postal code format is correct");
         } catch (PostalCodeException e) {
             System.out.println(e.getMessage());
         }
--- src/com/sda/she_likes_java/homework/Exercise29/PostalCodeChecker.java
+++ src/com/sda/she_likes_java/homework/Exercise29/PostalCodeChecker.java
@@ -2,7 +2,7 @@ package com.sda.she_likes_java.homework.Exercise29;
 
 public class PostalCodeChecker {
     public static void checkPostalCode(PostalCode postalCode) throws PostalCodeException{
-        String postalCodePattern = "^LV\\d{4}$";
+        String postalCodePattern = "^LV-\\d{4}$";
 
         if (!postalCode.getPostalCode().matches(postalCodePattern)){
             throw new PostalCodeException("Invalid postal code format");
