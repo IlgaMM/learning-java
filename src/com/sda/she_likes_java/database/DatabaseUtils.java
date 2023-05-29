f0e44d8bb45278d837f7608f46c688051767556c
Momi.lv <ilga.miezaine@gmail.com>
2023-05-29 16:30:45 +0300

    sql

--- src/com/sda/she_likes_java/database/DatabaseUtils.java
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -5,7 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtils {
-
+    /**
+     * Script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     *Script for inserting one record (row) into table
+     * INSERT INTO
+     * TEST(ID,NAME)
+     * VALUES(1,'Ilga');
+     *
+     * Script for updating data
+     * UPDATE TEST -- name of table to update
+     * SET NAME = 'Alma' -- column value to change
+     * WHERE ID=2; -- selected row
+     */
+//handle internally
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
@@ -17,7 +32,7 @@ public class DatabaseUtils {
     }
 
     public static Connection getConnectionButSometimesThrowException() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
+        return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
                 "sa",
                 "");
     }
