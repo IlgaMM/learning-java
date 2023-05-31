a62ece6a3777275073c7c078bf964ddc362610a4
Momi.lv <ilga.miezaine@gmail.com>
2023-05-31 21:44:25 +0300

    Crud repository for Accenture Student

--- src/com/sda/she_likes_java/database_in_memory/Main.java
+++ src/com/sda/she_likes_java/database_in_memory/Main.java
@@ -1,9 +1,9 @@
 package com.sda.she_likes_java.database_in_memory;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
         Connection connection = DbConnectionUtils.getConnection(DbCredentials.h2InMemoryUrl,
                 DbCredentials.h2Username, DbCredentials.h2Password);
@@ -11,5 +11,13 @@ public class Main {
         System.out.println("Do we have connection? " + (connection != null));
 
         DbInit.prepareData(connection);
+
+        System.out.println("Let's read all students");
+        AccentureStudentsRepository repository = new AccentureStudentsRepository(connection);
+
+        List<AccentureStudent> students = repository.getAllStudents();
+        for (AccentureStudent student : students) {
+            System.out.println("Student: " + student);
+        }
     }
 }
