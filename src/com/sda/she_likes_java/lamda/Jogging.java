216a888a424417b8949813457eaec7e0e6d082e5
Momi.lv <ilga.miezaine@gmail.com>
2023-06-05 19:57:47 +0300

    Lambda introduction

---
+++ src/com/sda/she_likes_java/lamda/Jogging.java
@@ -0,0 +1,7 @@
+package com.sda.she_likes_java.lamda;
+
+// API - Application programming interface
+// contract - what to do
+public interface Jogging {
+    void run();
+}
---
+++ src/com/sda/she_likes_java/lamda/SlowRunner.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.lamda;
+
+public class SlowRunner implements Jogging {
+    @Override
+    public void run() {
+        System.out.println("I'm terribly slow...");
+    }
+}
