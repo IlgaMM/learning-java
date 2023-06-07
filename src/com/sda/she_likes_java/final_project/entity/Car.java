68f94fd759269b1419cdb4c910febc6cc0803a95
Momi.lv <ilga.miezaine@gmail.com>
2023-06-07 21:36:13 +0300

    Project structure in progress

---
+++ src/com/sda/she_likes_java/final_project/entity/Car.java
@@ -0,0 +1,6 @@
+package com.sda.she_likes_java.final_project.entity;
+
+
+//TODO: add fields
+public record Car(Category category) {
+}
---
+++ src/com/sda/she_likes_java/final_project/repository/BookingRecordRepository.java
@@ -0,0 +1,9 @@
+package com.sda.she_likes_java.final_project.repository;
+
+public class BookingRecordRepository {
+
+    public void bookCar() {
+
+    }
+
+}
